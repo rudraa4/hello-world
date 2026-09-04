ef2bcb760956733c6e0ba5ef7f9b7b4553b3ccff
Rudra <rudraa.2180@gmail.com>
2026-09-05 00:03:32 +0530

    Reformat good morning.py for readability

--- good morning.py
+++ good morning.py
@@ -1,14 +1,20 @@
 import time 
+
 time_hour = time.strftime('%H')
 time_minute = time.strftime('%M')
 time_second = time.strftime('%S')
+
 name = input("Enter your name: " )
+
 if (4<=int(time_hour)<=11):
     print("Good morning", name)
+    
 elif(12<=int(time_hour)<=16):
     print("Good afternoon", name)
+    
 elif(17<=int(time_hour)<=21):
     print("Good evening", name)
+    
 else:
     print("you should sleep now", name,"its too late!")
     
